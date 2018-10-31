e7b0ca67c3a8dea51decb21e6e5098d68d561587
Jason <me@jasonharrison.us>
2018-10-30 22:42:42 -0400

    Fix indentation

--- src/App.tsx
+++ src/App.tsx
@@ -17,19 +17,19 @@ class App extends React.Component {
   public render() {
     return (
       <MuiThemeProvider theme={this.theme}>
-      <div>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" color="inherit">
-              Bill Split
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <div style={{maxWidth: '500px', marginLeft: 'auto', marginRight: 'auto', paddingTop: '32px'}}>
-          <BillSplit />
+        <div>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" color="inherit">
+                Bill Split
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <div style={{maxWidth: '500px', marginLeft: 'auto', marginRight: 'auto', paddingTop: '32px'}}>
+            <BillSplit />
+          </div>
         </div>
-      </div>
-      </MuiThemeProvider>
+        </MuiThemeProvider>
     );
   }
 }
