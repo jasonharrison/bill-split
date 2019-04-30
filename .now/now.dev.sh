a21badca2da24c9cf8856f2f18628d9395bfe046
Jason <me@jasonharrison.us>
2019-04-30 13:43:09 -0400

    Delete .now/

--- .now/now.dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-CLEAN_BRANCH_NAME=${CIRCLE_BRANCH//\//-};
-CIRCLE_SHA1=${CIRCLE_SHA1//\//-};
-
-sed -i -e "s/null; \/\/ for CircleCI/\"$CLEAN_BRANCH_NAME ${CIRCLE_SHA1:0:7}\";/" src/App.tsx
-sed -i -e "s/<meta charset/<p hidden style=\"display:none\"\>Version: $CLEAN_BRANCH_NAME $CIRCLE_SHA1<\/p\>\\n    <meta charset/" public/index.html
--- .now/now.master.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-CLEAN_BRANCH_NAME=${CIRCLE_BRANCH//\//-};
-CIRCLE_SHA1=${CIRCLE_SHA1//\//-};
-
-sed -i -e "s/<meta charset/<p hidden style=\"display:none\"\>Version: $CLEAN_BRANCH_NAME $CIRCLE_SHA1<\/p\>\\n    <meta charset/" public/index.html
