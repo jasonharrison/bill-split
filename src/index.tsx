ee4a28cc4a7ebb96d5f3277a38d2cf0a171f53ee
Jason <me@jasonharrison.us>
2019-05-04 20:27:49 -0400

    WIP Routing / redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,8 @@ import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from "react-redux";
-import store from "./store/index";
+import { Provider } from 'react-redux';
+import { store } from './redux';
 
 ReactDOM.render((
   <Provider store={store}>
